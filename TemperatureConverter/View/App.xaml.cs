2b305f3c8ee8c08c199eaa51d1a13c23a1a926ee
r0661011 <r0661011@student.ucll.be>
2020-04-12 15:04:46 +0200

    commit

--- TemperatureConverter/View/App.xaml.cs
+++ TemperatureConverter/View/App.xaml.cs
@@ -14,4 +14,5 @@ namespace View
     public partial class App : Application
     {
     }
+
 }
--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -49,7 +50,25 @@ namespace View
             var celsiusString = celsius.ToString();
 
             fahrenheitTextBox.Text = fahrenheitString;
-            celsiusTextBox.Text = celsiusString;
+        }
+    }
+
+    public class CelsiusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var kelvin = (double)value;
+            var celsius = kelvin - 273.15;
+
+            return celsius.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var celsius = double.Parse((string)value);
+            var kelvin = celsius + 273.15;
+
+            return kelvin;
         }
     }
 }
